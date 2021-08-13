e325dd7cc3e752da8acb5ee59cd68777d6b50c73
Katja Zoner <katja.zoner@pennmedicine.upenn.edu>
2021-08-13 11:19:54 -0400

    Added er40 events tsv, added QSM, added fmri dwi dir-AP, added filtering by timestamp.

--- scripts/curation/heuristic.py
+++ scripts/curation/heuristic.py
@@ -1,7 +1,7 @@
 '''
 Heuristic to curate the Evolution_833922 project.
 Katja Zoner
-Updated: 04/27/2021
+Updated: 05/10/2021
 '''
 
 import os
@@ -27,7 +27,7 @@ fmap_fmriPA = create_key(
 rest = create_key(
     'sub-{subject}/{session}/func/sub-{subject}_{session}_task-rest_dir-AP_bold')
 er40 = create_key(
-    'sub-{subject}/{session}/func/sub-{subject}_{session}_task-e40_dir-AP_bold')
+    'sub-{subject}/{session}/func/sub-{subject}_{session}_task-er40_dir-AP_bold')
 socialapproach = create_key(
     'sub-{subject}/{session}/func/sub-{subject}_{session}_task-socialapproach_dir-AP_bold')
 
@@ -39,15 +39,21 @@ perf = create_key(
 dwi = create_key(
     'sub-{subject}/{session}/dwi/sub-{subject}_{session}_dwi')
 
-# Field maps - dwi ??? two PA scans?
+# Field maps - dwi 
 fmap_dwiAP= create_key(
     'sub-{subject}/{session}/fmap/sub-{subject}_{session}_acq-dwi_dir-AP_epi')
 fmap_dwiPA = create_key(
     'sub-{subject}/{session}/fmap/sub-{subject}_{session}_acq-dwi_dir-PA_epi')
 
-# T2star weighted
-t2starw = create_key(
-    'sub-{subject}/{session}/anat/sub-{subject}_{session}_T2starw')
+#t2starw = create_key(
+#    'sub-{subject}/{session}/anat/sub-{subject}_{session}_T2starw')
+
+# QSM
+## NOTE: Not a part of the BIDS Standard yet! --> add to .bidsignore
+qsm_ph = create_key(
+    'sub-{subject}/{session}/swi/sub-{subject}_{session}_echo-{item}_part-phase_GRE')
+qsm_mag = create_key(
+    'sub-{subject}/{session}/swi/sub-{subject}_{session}_echo-{item}_part-mag_GRE')
 
 # T2w
 t2w = create_key(
@@ -56,6 +62,7 @@ t2w = create_key(
 # Old T2w_ABCD sequence (present for several early subjects)
 t2w_ABCD = create_key(
     'sub-{subject}/{session}/anat/sub-{subject}_{session}_acq-ABCD_T2w')
+
 ############################ Define heuristic rules ############################
 
 def infotodict(seqinfo):
@@ -66,9 +73,12 @@ def infotodict(seqinfo):
     seqitem: run number during scanning
     subindex: sub index within group
     """
+    
+    import pandas as pd
 
-    #last_run = len(seqinfo)
-
+    # Curation Round 1 cutoff date -- not yet curating sessions added after this date.
+    DATE_CUTOFF = pd.Timestamp('2021-04-23')
+    
     # Info dictionary to map series_id's to correct create_key key
     info = {
         t1w: [], 
@@ -76,20 +86,42 @@ def infotodict(seqinfo):
         fmap_fmriAP: [], fmap_fmriPA: [],
         fmap_dwiAP: [], fmap_dwiPA: [],
         perf: [], dwi: [],
-        t2starw: [], t2w: [], t2w_ABCD: []
+        #t2starw: [], 
+        qsm_ph: [], qsm_mag: [],
+        t2w: [], t2w_ABCD: []
         }
+
+    # List of series_descriptions to not include
+    ignore_list = [
+        'anat-scout',
+        'anat-vnavsetter_acq-BC-T1w_seq-ABCD',
+        'anat_acq-vnavsetter_T2w',
+        'Perfusion_Weighted'
+    ]
 
     def get_latest_series(key, s):
         info[key].append(s.series_id)
 
     for s in seqinfo:
-        protocol = s.protocol_name.lower()
 
-        # T1w structural scans
+        # If pilot subject, skip and don't add to BIDS
+        if "pilot" in s.patient_id.lower():
+            print(f'Skipping pilot subject: {s.patient_id}')
+            continue
+        
+        # NOTE: For Curation Round 1, only process sessions that occurred before DATE_CUTOFF (2021-04-23)
+        timestamp = pd.Timestamp(s.date)
+        if timestamp > DATE_CUTOFF:
+            continue
+
+        protocol = s.protocol_name.lower()
+        series_description=s.series_description.lower()
+
+        # T1w
         if "mprage" in protocol and "navsetter" not in s.series_description:
             get_latest_series(t1w, s)
 
-        # fMRI scans
+        # fMRI 
         elif "task-rest" in protocol:
             get_latest_series(rest, s)
         elif "task-er40" in protocol:
@@ -97,7 +129,7 @@ def infotodict(seqinfo):
         elif "task-socialapproach" in protocol:
             get_latest_series(socialapproach, s)
         
-        # Fieldmap scans
+        # Fieldmap
         elif "fmap" in protocol:
             # fMRI fmaps
             if "acq-fmri" in protocol:
@@ -107,13 +139,10 @@ def infotodict(seqinfo):
                     get_latest_series(fmap_fmriPA, s)
             # dwi fmaps
             elif "acq-dwi" in protocol:
-                # PhaseEncodingDirection= 'j-' --> PA
-                # PhaseEncodingDirection= 'j' --> AP
-                ## TODO: adjust for scans w/out PhaseEncodingDirection field
-                if "-" in s.PhaseEncodingDirection:
-                    get_latest_series(fmap_dwiPA, s)
-                else: 
+                if "dir-ap" in protocol:
                     get_latest_series(fmap_dwiAP, s)
+                elif "dir-pa" in protocol:
+                    get_latest_series(fmap_dwiPA, s)
         
         # perf
         elif "pcasl" in protocol and not s.is_derived:
@@ -123,9 +152,14 @@ def infotodict(seqinfo):
         elif "dwi-multishell" in protocol:
             get_latest_series(dwi, s)
 
-        elif "t2star" in protocol:
-            get_latest_series(t2starw, s)
+        # qsm
+        elif "qsm" in protocol:
+            if "P" in s.image_type:
+                get_latest_series(qsm_ph, s)
+            elif "M" in s.image_type:
+                get_latest_series(qsm_mag, s)
 
+        # t2w
         elif "t2w" in protocol and "navsetter" not in s.series_description:
             if "abcd" in protocol:
                 get_latest_series(t2w_ABCD, s)
@@ -133,32 +167,26 @@ def infotodict(seqinfo):
             elif "spc" not in protocol:
                 get_latest_series(t2w, s)
 
-        else:
+        # Don't print "not recognized" message for scans we're intentionally not including.
+        elif s.series_description not in ignore_list:
             print("Series not recognized!: ", s.protocol_name, s.dcm_dir_name)
 
     return info
 
 ################## Hardcode required params in MetadataExtras ##################
 ## TODO: no clu ;(
-'''
+
 MetadataExtras = {    
     perf: {
         "ArterialSpinLabelingType": "PCASL",
-
-        "BackgroundSuppression": False, # required
-        "InterPulseSpacing": 4,
-        "LabelingDistance": 2,
-        "LabelingDuration": 1.2, # required 
-        "LabelingEfficiency": 0.72,
-        "LabelingSlabLocation": "X", # correct
-        "LabelingType": "PCASL", #?? unsure
-        "M0Type": "Absent", # required 
-        "PostLabelingDelay": 1.517, # required 2.0 us
-        "PulseSequenceType": "2D",
-        "RepetitionTimePreparation": 0, # required
+        "BackgroundSuppression": True,
+        "LabelingDuration": 0.7, # TODO: which is correct?? From Azeez: 0.7s, from Mark: 1.5s 
+        "M0Type": "Included", 
+        "PostLabelingDelay": 2,
+        "RepetitionTimePreparation": 4.3 # not required?
+        #"TotalAcquiredPairs": 7? 8?    # says required?
     }
 }
-'''
 
 # Should any other scans be listed here, or just fmri/dwi's?
 IntendedFor = {
@@ -172,19 +200,22 @@ IntendedFor = {
         '{session}/func/sub-{subject}_{session}_task-er40_dir-AP_bold.nii.gz',
         '{session}/func/sub-{subject}_{session}_task-socialapproach_dir-AP_bold.nii.gz'
     ],
+    fmap_dwiAP: [
+        '{session}/dwi/sub-{subject}_{session}_dwi.nii.gz'
+    ],
     fmap_dwiPA: [
         '{session}/dwi/sub-{subject}_{session}_dwi.nii.gz'
     ]
 }
 
 # TODO: Need to get events tsv files
-'''
+# TODO: Need to finalize aslcontext number/order of volumes
 def AttachToSession():
-    NUM_VOLUMES=40
+    NUM_VOLUMES=7
     data = ['control', 'label'] * NUM_VOLUMES
     data = '\n'.join(data)
-    data = 'volume_type\n' + data # the data is now a string; perfect!
-
+    data = 'volume_type\n' + 'm0scan\n' + data 
+    
     # define asl_context.tsv file
     asl_context = {
         'name': 'sub-{subject}/{session}/perf/sub-{subject}_{session}_aslcontext.tsv',
@@ -194,25 +225,17 @@ def AttachToSession():
 
     import pandas as pd 
 
-    df = pd.read_csv("info/task-idemo_events.tsv", sep='\t') 
+    df = pd.read_csv("info/task-er40_events.tsv", sep='\t') 
 
-    # define idemo events.tsv file
-    idemo_events = {
-        'name': 'sub-{subject}/{session}/func/sub-{subject}_{session}_task-idemo_events.tsv',
-        'data': df.to_csv(index=False, sep='\t'),
-        'type': 'text/tab-separated-values'
-    }
-
-    
-    # define jolo events.tsv file
-    jolo_events = {
-        'name': 'sub-{subject}/{session}/func/sub-{subject}_{session}_task-jolo_events.tsv',
+    # define er40 events.tsv file
+    er40_events = {
+        'name': 'sub-{subject}/{session}/func/sub-{subject}_{session}_task-er40_dir-AP_events.tsv',
         'data': df.to_csv(index=False, sep='\t'),
         'type': 'text/tab-separated-values'
     }
     
-    return [asl_context, idemo_events]
-'''
+    return [asl_context,  er40_events]
+
 
 ####################### Rename session and subject labels #######################
 
@@ -241,7 +264,7 @@ def gather_session_indices():
             sessions = sorted(sessions, key=lambda x: x.timestamp)
             # loop through the subject's sessions, assign each session an index
             for i, sess in enumerate(sessions):
-                session_labels[sess.label] = "EVO" + str(i + 1)
+                session_labels[sess.label] = "EVOL" + str(i + 1)
 
     return session_labels
 
